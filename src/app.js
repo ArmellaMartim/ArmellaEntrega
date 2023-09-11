675b5cee02f31802735212abc2e5cb0b1bcfd3ae
Martim Armella <armellam7@gmail.com>
2023-09-11 13:11:43 -0300

    product

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import { fileURLToPath} from 'url';
+import path, { dirname } from 'path';
+import { createRequire } from 'module';
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.listen(8080, ()=> console.log("Servidor en puerto 8080"));
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
 
